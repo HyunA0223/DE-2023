c626077e36386fa23ae960f063320b84dda58786
HyunA Lee <86672939+HyunA0223@users.noreply.github.com>
2023-05-16 12:07:40 +0900

    Update IMDBStudent20191003.java

--- HW1/IMDBStudent20191003.java
+++ HW1/IMDBStudent20191003.java
@@ -9,64 +9,53 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
-​
-public class IMDBStudent20191003 {
-​
-	public static class IMDBStudent20191003Mapper extends Mapper<Object, Text, Text, IntWritable>
-	{
+
+public class IMDBStudent20191003
+{
+	public static class IMDBStudent20191003Mapper extends Mapper<Object, Text, Text, IntWritable>{
 		private final static IntWritable one = new IntWritable(1);
-		private Text genreText = new Text();
-​
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
-		{​
-            		String[] line =  value.toString().split("::");
+		private Text mapResult = new Text();
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] line = value.toString().split("::");
 			int len = line.length;
-            		/*String[] genres = line[2].split("|");
-         
-            		for (String genre : genres) {
-               			genreText.set(genre);
-                		context.write(genreText, one);
-            		}*/
 			StringTokenizer str = new StringTokenizer(line[len - 1], "|");
+			
 			while (str.hasMoreTokens()) {
-				genreText.set(str.nextToken());
-				context.write(genreText, one);
+				mapResult.set(str.nextToken());
+				context.write(mapResult, one);
 			}
+		}	
+	}
+	
+	public static class IMDBStudent20191003Reducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+		private IntWritable result = new IntWritable();
+		
+		public void reduce(Text key, Iterable<IntWritable> values, Context context ) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
 		}
 	}
-​
-	public static class IMDBStudent20191003Reducer extends Reducer<Text, IntWritable, Text, IntWritable>
-	{
-		private IntWritable sumWritable = new IntWritable();
-​
-		public void reduce(Text key, Iterable<IntWritable> values, Context context ) throws IOException, InterruptedException
-		{
-            		int sum = 0;
-            		for (IntWritable val : values) {
-                		sum += val.get();
-            		}
-            		sumWritable.set(sum);
-            		context.write(key, sumWritable);
-		}
-	}
-​
-	public static void main(String[] args) throws Exception
-	{
+
+	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		Job job = new Job(conf, "IMDBStudent20191003");
-​
+		Job job = new Job(conf, "imdbstudent20191003");
+		
 		job.setJarByClass(IMDBStudent20191003.class);
-		job.setMapperClass(IMDBIMDBStudent20191003Mapper.class);
+		job.setMapperClass(IMDBStudent20191003Mapper.class);
 		job.setCombinerClass(IMDBStudent20191003Reducer.class);
 		job.setReducerClass(IMDBStudent20191003Reducer.class);
-​
+		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-​
+		
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		FileSystem.get(job.getConfiguration()).delete( new Path(args[1]), true);
-        
 		job.waitForCompletion(true);
 	}
 }
